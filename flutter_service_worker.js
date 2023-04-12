8cf51bd06fc7f304bb0d747983ab93900891d8b4
Jesus Acosta <jesusgabriel.91@gmail.com>
2023-04-12 01:01:21 -0400

    push update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5850d3f45ae132d7729a1b8955052030",
-"/": "5850d3f45ae132d7729a1b8955052030",
+"index.html": "92b08b1455e9209b08eef313c09a9c44",
+"/": "92b08b1455e9209b08eef313c09a9c44",
 "main.dart.js": "7f5c69d9923cf55b9ab6ea7a0e754b83",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
